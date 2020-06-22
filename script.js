c51d9ab075df0e38a33957527be51986374a0243
Aaron Slama <aslama@mp.ics.com>
2020-06-22 13:31:12 -0500

    Be specific with quantity

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-var str1 = "5 + 5";
-console.log(str1);
-var num1 = 5 + 5;
-console.log(num1);
+var array1 = ["Hello", "world!", 67, true, "hi"];
+//unlimited amount of elements
+var array2 = ["Hello", "World", "again"];
+//limisted amount of elements based on what is between the brackets []
+var tuple3 = ["some new string", "Second property", 67, true];
+console.log(array1);
+console.log(array2);
+console.log(tuple3);
+var tuple4;
+tuple4 = ["this is the ", "4th tuple"];
+var tuple5;
+tuple5 = ["Where were they going", "without ever knowing the way", 67];
+tuple5.push("1", "2", 3);
+console.log(tuple5);
--- script.ts
+++ script.ts
@@ -1,6 +1,21 @@
-var str1:String = "5 + 5";
-console.log(str1);
+var array1 = ["Hello", "world!", 67, true, "hi"];
 
-var num1:number = 5 + 5;
 
-console.log(num1); 
+//unlimited amount of elements
+var array2: string[]  = ["Hello", "World", "again"];
+
+//limisted amount of elements based on what is between the brackets []
+var tuple3:[string,string,number,boolean] = ["some new string", "Second property",67, true];
+console.log(array1);
+console.log(array2);
+console.log(tuple3);
+
+var tuple4: string[];
+
+tuple4 = ["this is the ", "4th tuple"]; 
+
+var tuple5: [string, string, number];
+
+tuple5 = ["Where were they going", "without ever knowing the way", 67]
+tuple5.push("1","2",3);
+console.log(tuple5);
