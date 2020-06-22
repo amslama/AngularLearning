a19dd98161d836160d8f019fed3673e3c36cdbf3
Aaron Slama <aslama@mp.ics.com>
2020-06-22 12:49:09 -0500

    Beginnings

---
+++ script.js
@@ -0,0 +1,4 @@
+var str1 = "5 + 5";
+console.log(str1);
+var num1 = 5 + 5;
+console.log(num1);
---
+++ script.ts
@@ -0,0 +1,6 @@
+var str1:String = "5 + 5";
+console.log(str1);
+
+var num1:number = 5 + 5;
+
+console.log(num1); 
