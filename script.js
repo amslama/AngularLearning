b6b2e035cb1a0c41d1c7b2f2669fa8a636435a9b
Aaron Slama <aslama@mp.ics.com>
2020-06-22 14:48:18 -0500

    established base class

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-var str1 = "5 + 5";
-console.log(str1);
-var num1 = 5 + 5;
-console.log(num1);
+var human = /** @class */ (function () {
+    function human() {
+    }
+    human.prototype.doAction = function () { };
+    human.prototype.getName = function () {
+        console.log("The first name is " + this.firstName);
+    };
+    human.prototype.getAllData = function () {
+        this.getName();
+        console.log("The last name is " + this.lastName);
+        console.log("The eye color is " + this.eyeColor);
+        console.log("The hair color is " + this.hairColor);
+        console.log("The age is " + this.age);
+    };
+    return human;
+}());
+var test = new human();
+test.firstName = "Maria";
+test.lastName = "Boris";
+test.getName();
+var test2 = new human();
+test2.firstName = "Mark";
+test2.lastName = "Johnson";
+test2.age = 28;
+test2.hairColor = "brown";
+test2.eyeColor = "blue";
+test2.getAllData();
--- script.ts
+++ script.ts
@@ -1,6 +1,37 @@
-var str1:String = "5 + 5";
-console.log(str1);
+class human{
+    //field == property == variable
+    firstName: string;
+    lastName: string;
+    eyeColor: string;
+    hairColor: string;
+    age:number;
 
-var num1:number = 5 + 5;
+    doAction(){}
 
-console.log(num1); 
+    getName(){
+        console.log("The first name is " + this.firstName);
+    }
+
+    getAllData(){
+        this.getName();
+        console.log("The last name is " + this.lastName);
+        console.log("The eye color is " + this.eyeColor);
+        console.log("The hair color is " + this.hairColor);
+        console.log("The age is " + this.age);
+    }
+}
+
+var test = new human();
+test.firstName = "Maria";
+test.lastName = "Boris";
+
+test.getName();
+
+var test2 = new human();
+test2.firstName = "Mark";
+test2.lastName = "Johnson";
+test2.age = 28;
+test2.hairColor = "brown";
+test2.eyeColor = "blue";
+
+test2.getAllData();
