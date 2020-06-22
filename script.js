526d0ff0fe3531c090c6cc8bc5e5bad70d53373b
Aaron Slama <aslama@mp.ics.com>
2020-06-22 16:24:16 -0500

    "Section 3, video 2"

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-var str1 = "5 + 5";
-console.log(str1);
-var num1 = 5 + 5;
-console.log(num1);
+var class1 = /** @class */ (function () {
+    function class1(name, age, hairColor) {
+        this.name = name;
+        this.age = age;
+        this.hairColor = hairColor;
+    }
+    class1.prototype.func1 = function () {
+        console.log("yay;");
+    };
+    return class1;
+}());
+var obj1 = new class1("John", 47, "Brown");
+console.log(obj1);
+obj1.func1();
--- script.ts
+++ script.ts
@@ -1,6 +1,24 @@
-var str1:String = "5 + 5";
-console.log(str1);
+interface CLinterface1{
+    name: string;
 
-var num1:number = 5 + 5;
+}
+interface CLinterface2{
+    age: number;
+    func1();
+}
 
-console.log(num1); 
+class class1 implements CLinterface1, CLinterface2
+{
+    constructor(public name: string, public age: number,public hairColor:string){}
+    func1()
+    {
+        console.log("yay;")
+    }
+}
+
+var obj1 =  new class1("John", 47, "Brown");
+console.log(obj1);
+
+obj1.func1();
+
+var obj2:CLinterface1 = new class1("Mary", 21, "pink");
