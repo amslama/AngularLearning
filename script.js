b953cd15afd59b9fb7b313f1ef842a8f62e5ac72
Aaron Slama <aslama@mp.ics.com>
2020-06-22 13:14:57 -0500

    finsihed, optionals are cool

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-var str1 = "5 + 5";
-console.log(str1);
-var num1 = 5 + 5;
-console.log(num1);
+//Optional parameters MUST be defined in the end
+function func1(para1, para2, para3, para4, para5) {
+    if (para5 === void 0) { para5 = "default value"; }
+    console.log("para1 = " + para1);
+    console.log("para2 = " + para2);
+    console.log("para3 = " + para3);
+    console.log("para4 = " + para4);
+    console.log("para5 = " + para5);
+}
+func1("The string parameter", 67, ["bob", "for", "apples"], [6, 7, 8, 9]);
+func1("The string parameter", 67, ["bob", "for", "apples"], [6, 7, 8, 9], "It is defined");
--- script.ts
+++ script.ts
@@ -1,6 +1,11 @@
-var str1:String = "5 + 5";
-console.log(str1);
 
-var num1:number = 5 + 5;
-
-console.log(num1); 
+//Optional parameters MUST be defined in the end
+function func1(para1:string, para2:number, para3:string[], para4:number[], para5:string = "default value"){
+    console.log("para1 = " + para1);
+    console.log("para2 = " + para2);
+    console.log("para3 = " + para3);
+    console.log("para4 = " + para4);
+    console.log("para5 = " + para5);
+}
+func1("The string parameter", 67, ["bob", "for", "apples"], [6,7,8,9]);
+func1("The string parameter", 67, ["bob", "for", "apples"], [6,7,8,9], "It is defined");
