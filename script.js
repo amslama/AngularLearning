8993d2c5a05d8ded33c7e3870f229845b20087d4
Aaron Slama <aslama@mp.ics.com>
2020-06-23 08:24:42 -0500

    Method overloading

--- script.js
+++ script.js
@@ -1,7 +1,53 @@
-var class2 = /** @class */ (function () {
-    function class2() {
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var person = /** @class */ (function () {
+    function person(name) {
+        this.name = name;
     }
-    return class2;
+    person.prototype.eating = function (food) {
+        if (food === void 0) { food = "none"; }
+        console.log(this.name + " eat s" + food);
+    };
+    return person;
 }());
-var obj1 = { age: 32, name: "Barry" };
-console.log(obj1);
+var Obj1class = /** @class */ (function (_super) {
+    __extends(Obj1class, _super);
+    function Obj1class(name) {
+        return _super.call(this, name) || this;
+    }
+    Obj1class.prototype.eating = function (food) {
+        if (food === void 0) { food = "pasta"; }
+        console.log("obj1class");
+        _super.prototype.eating.call(this, food);
+    };
+    return Obj1class;
+}(person));
+var Obj2class = /** @class */ (function (_super) {
+    __extends(Obj2class, _super);
+    function Obj2class(name) {
+        return _super.call(this, name) || this;
+    }
+    Obj2class.prototype.eating = function (food) {
+        if (food === void 0) { food = "Macaroni"; }
+        console.log("obj2class");
+        _super.prototype.eating.call(this, food);
+    };
+    return Obj2class;
+}(person));
+var obj1 = new person("John");
+obj1.eating();
+var obj1class = new Obj1class("Mar");
+obj1class.eating();
+var obj2class = new Obj2class("Martha");
+obj2class.eating();
--- script.ts
+++ script.ts
@@ -1,16 +1,48 @@
-interface CLinterface{
-    name: string;
+class person {
+    constructor(public name:string)
+    {
+
+    }
+
+    eating(food = "none")
+    {
+        console.log(this.name + " eats " + food);
+    }
+
 }
 
-class class2 implements CLinterface {
-    name: string;
+class Obj1class extends person
+{
+    constructor(name:string){
+        super(name);
+    }
+
+    eating(food = "pasta")
+    {
+        console.log("obj1class");
+        super.eating(food);
+    }
+}
+class Obj2class extends person
+{
+    constructor(name:string){
+        super(name);
+    }
+
+    eating(food = "Macaroni")
+    {
+        console.log("obj2class");
+        super.eating(food);
+    }
 }
 
-interface Interface1 extends class2{
-    age:number;
+var obj1 = new person("John");
 
-}
+obj1.eating();
 
-var obj1: Interface1 = {age: 32, name: "Barry"}
+var obj1class = new Obj1class("Mar");
 
-console.log(obj1);
+obj1class.eating();
+
+var obj2class = new Obj2class("Martha");
+obj2class.eating();
